c4f0a5f748cbba3e93afb975fb37fe104199fc37
oscarnar <orosas58@gmail.com>
2020-12-16 10:05:48 -0500

    Aplicando analisis estatico

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -1,45 +1,42 @@
-var Author = require('../models/author')
-var async = require('async')
-var Book = require('../models/book')
-
-const { body,validationResult } = require("express-validator");
+const async = require('async');
+const { body, validationResult } = require('express-validator');
+const Author = require('../models/author');
+const Book = require('../models/book');
 
 // Display list of all Authors.
 exports.author_list = function (req, res, next) {
-
     Author.find()
         .sort([['family_name', 'ascending']])
-        .exec(function (err, list_authors) {
+        .exec((err, listAuthors) => {
             if (err) { return next(err); }
             // Successful, so render.
-            res.render('author_list', { title: 'Author List', author_list: list_authors });
-        })
-
+            res.render('author_list', { title: 'Author List', author_list: listAuthors });
+            return null;
+        });
 };
 
 // Display detail page for a specific Author.
 exports.author_detail = function (req, res, next) {
-
     async.parallel({
-        author: function (callback) {
+        author: (callback) => {
             Author.findById(req.params.id)
-                .exec(callback)
+                .exec(callback);
         },
-        authors_books: function (callback) {
-            Book.find({ 'author': req.params.id }, 'title summary')
-                .exec(callback)
+        authors_books: (callback) => {
+            Book.find({ author: req.params.id }, 'title summary')
+                .exec(callback);
         },
-    }, function (err, results) {
+    }, (err, results) => {
         if (err) { return next(err); } // Error in API usage.
         if (results.author == null) { // No results.
-            var err = new Error('Author not found');
-            err.status = 404;
-            return next(err);
+            const myErr = new Error('Author not found');
+            myErr.status = 404;
+            return next(myErr);
         }
         // Successful, so render.
         res.render('author_detail', { title: 'Author Detail', author: results.author, author_books: results.authors_books });
+        return null;
     });
-
 };
 
 // Display Author create form on GET.
@@ -51,27 +48,29 @@ exports.author_create_get = function (req, res, next) {
 exports.author_create_post = [
 
     // Validate and sanitize fields.
-    body('first_name').trim().isLength({ min: 1 }).escape().withMessage('First name must be specified.')
-        .isAlphanumeric().withMessage('First name has non-alphanumeric characters.'),
-    body('family_name').trim().isLength({ min: 1 }).escape().withMessage('Family name must be specified.')
-        .isAlphanumeric().withMessage('Family name has non-alphanumeric characters.'),
+    body('first_name').trim().isLength({ min: 1 }).escape()
+    .withMessage('First name must be specified.')
+    .isAlphanumeric()
+    .withMessage('First name has non-alphanumeric characters.'),
+    body('family_name').trim().isLength({ min: 1 }).escape()
+    .withMessage('Family name must be specified.')
+    .isAlphanumeric()
+    .withMessage('Family name has non-alphanumeric characters.'),
     body('date_of_birth', 'Invalid date of birth').optional({ checkFalsy: true }).isISO8601().toDate(),
     body('date_of_death', 'Invalid date of death').optional({ checkFalsy: true }).isISO8601().toDate(),
 
     // Process request after validation and sanitization.
     (req, res, next) => {
-
         // Extract the validation errors from a request.
         const errors = validationResult(req);
-        
         // Create Author object with escaped and trimmed data
-        var author = new Author(
+        const author = new Author(
             {
                 first_name: req.body.first_name,
                 family_name: req.body.family_name,
                 date_of_birth: req.body.date_of_birth,
                 date_of_death: req.body.date_of_death,
-            }
+            },
         );
 
         if (!errors.isEmpty()) {
