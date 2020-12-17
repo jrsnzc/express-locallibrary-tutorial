19d788aa68a1a79f858e2f6e59e3d54f89baa41d
Jerson Zúñiga-Coayla <jzunigacoayla@gmail.com>
2020-12-17 11:44:54 -0500

    Directly call to mongoose.Schema

--- models/genre.js
+++ models/genre.js
@@ -1,19 +1,20 @@
 const mongoose = require('mongoose');
 
-const Schema = mongoose.Schema;
-
-const GenreSchema = new Schema({
-    name: {
-        type: String, required: true, minlength: 3, maxlength: 100,
-    },
+const GenreSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 3,
+    maxlength: 100,
+  },
 });
 
 // Virtual for this genre instance URL.
 GenreSchema
-.virtual('url')
-.get(function () {
-  return `/catalog/genre/${this.id}`;
-});
+  .virtual('url')
+  .get(function toString() {
+    return `/catalog/genre/${this._id}`;
+  });
 
 // Export model.
 module.exports = mongoose.model('Genre', GenreSchema);
