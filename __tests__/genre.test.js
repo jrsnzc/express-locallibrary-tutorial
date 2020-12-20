dab95ed46a9b4902962f9f3c2b9b144a3018fcf7
Jerson Zúñiga-Coayla <jzunigacoayla@gmail.com>
2020-12-20 18:30:45 -0500

    xUnit for CRUD operations over Genre Model

--- __tests__/genre.test.js
+++ __tests__/genre.test.js
@@ -1,6 +1,7 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 const server = require('../app');
+const Genre = require('../models/genre');
 
 const should = chai.should();
 
@@ -8,12 +9,42 @@ chai.use(chaiHttp);
 
 describe('Genres', () => {
   describe('Create a genre', () => {
-    it('It should not POST a genre with a name less than three characters', (done) => {
+    it('It should not add a genre with a name less than three characters', (done) => {
       const genre = {
         name: 'Ab',
       };
       chai.request(server)
         .post('/catalog/genre/create')
+        .send(genre)
+        .end((err, res) => {
+          res.should.have.status(200);
+          done();
+        });
+    });
+  });
+
+  describe('Update a genre', () => {
+    it('It should update a genre given the id', (done) => {
+      const genre = Genre({
+        name: 'Adventure',
+      });
+      chai.request(server)
+        .post('/catalog/genre/5fdfbaed5413ed1b3247bb0d')
+        .send(genre)
+        .end((err, res) => {
+          res.should.have.status(200);
+          done();
+        });
+    });
+  });
+
+  describe('Delete a genre', () => {
+    it('It should remove a genre given the id', (done) => {
+      const genre = {
+        id: '5fdfddbbfe6b671b04831097',
+      };
+      chai.request(server)
+        .post('/catalog/genre/5fdfddbbfe6b671b04831097/delete')
         .send(genre)
         .end((err, res) => {
           res.should.have.status(200);
